71fe2fd38159564b1c8ce71820837770ce72a281
ajoealex <ajoealex316@gmail.com>
2025-04-03 06:39:19 +0530

    added apps

--- tilesData.js
+++ tilesData.js
@@ -1,5 +1,7 @@
-// tilesData.js
+// tilesData.js  ajoealex/test-apps/apps/ajoealex/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator
 
 const tilesData = [
-    { name: "EventTester", link: "https://ajoealex.github.io/test-apps/apps/eventListener/" }
+    { name: "Event Tester", link: "https://ajoealex.github.io/test-apps/apps/eventListener/" },
+    { name: "ACCELQ Docker Run Command Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-docker-run-command-generator/" },
+    { name: "ACCELQ Web Driver Profile JSON Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator/" },
 ];
