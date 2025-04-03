a698c3c86f88c46be353db52078b6d6343b553b3
ajoealex <ajoealex316@gmail.com>
2025-04-03 07:09:43 +0530

    added new apps

--- tilesData.js
+++ tilesData.js
@@ -1,7 +1,11 @@
-// tilesData.js  ajoealex/test-apps/apps/ajoealex/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator
+// tilesData.js  ajoealex/test-apps/apps/accelq-system-resource-helper
 
 const tilesData = [
     { name: "Event Tester", link: "https://ajoealex.github.io/test-apps/apps/eventListener/" },
     { name: "ACCELQ Docker Run Command Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-docker-run-command-generator/" },
     { name: "ACCELQ Web Driver Profile JSON Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator/" },
+    { name: "ACCELQ System Resource Helper", link: "https://ajoealex.github.io/test-apps/apps/accelq-system-resource-helper/" },
+    { name: "Chromedriver Download Helper", link: "https://ajoealex.github.io/test-apps/apps/chromedriver-download-helper/" },
+    { name: "Fixed Width File Helper", link: "https://ajoealex.github.io/test-apps/apps/fixed-width-file-helper/" },
+    { name: "Appium Mobile Source Helper", link: "https://ajoealex.github.io/test-apps/apps/mobile-render/" }
 ];
