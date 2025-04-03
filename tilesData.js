c4ed89437914c20e536652f159d1315e13da0ab7
ajoealex <ajoealex316@gmail.com>
2025-04-03 09:10:48 +0530

    link update

--- tilesData.js
+++ tilesData.js
@@ -6,7 +6,7 @@ const tilesData = [
     { name: "ACCELQ Docker Run Command Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-docker-run-command-generator/" },
     { name: "ACCELQ Web Driver Profile JSON Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator/" },
     { name: "ACCELQ System Resource Helper", link: "https://ajoealex.github.io/test-apps/apps/accelq-system-resource-helper/" },
-    { name: "Chromedriver Download Helper", link: "https://chromedriver-download-helper.automationhub.in/" },
+    { name: "Chromedriver Download Helper", link: "https://ajoealex.github.io/chromedriver-download-helper/" },
     { name: "Fixed Width File Helper", link: "https://ajoealex.github.io/test-apps/apps/fixed-width-file-helper/" },
     { name: "Appium Mobile Source Helper", link: "https://ajoealex.github.io/test-apps/apps/mobile-render/" }
 ];
