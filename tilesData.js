635f58b28cf76b9efe16c7e8f201353cecef4b6d
ajoealex <ajoealex316@gmail.com>
2025-04-03 07:35:57 +0530

    added keyboard mouse event tester

--- tilesData.js
+++ tilesData.js
@@ -1,7 +1,8 @@
-// tilesData.js  ajoealex/test-apps/apps/accelq-system-resource-helper
+// tilesData.js
 
 const tilesData = [
-    { name: "Event Tester", link: "https://ajoealex.github.io/test-apps/apps/eventListener/" },
+    { name: "Element Event Tester", link: "https://ajoealex.github.io/test-apps/apps/eventListener/" },
+    { name: "Keyboard Mouse Event Tester", link: "https://ajoealex.github.io/test-apps/apps/keyboard_mouse_tester/" },
     { name: "ACCELQ Docker Run Command Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-docker-run-command-generator/" },
     { name: "ACCELQ Web Driver Profile JSON Generator", link: "https://ajoealex.github.io/test-apps/apps/ACCELQ-Web-Driver-Profile-Capability-Generator/" },
     { name: "ACCELQ System Resource Helper", link: "https://ajoealex.github.io/test-apps/apps/accelq-system-resource-helper/" },
