e821318159db7023d4695d3292222eb235aa7b33
Ajoe Alex <ajoealex316@gmail.com>
2026-08-10 13:23:25 +0530

    add api-test mock site

--- tilesData.js
+++ tilesData.js
@@ -18,5 +18,6 @@ const tilesData = [
     { name: "Markdown Live Preview Helper", link: "https://ajoealex.github.io/github-readme-viewer/" },
     { name: "Content Transform Kit", link: "https://ajoealex.github.io/transformkit/"},
     { name: "Json Path Finder", link: "https://ajoealex.github.io/json-path-finder/"},
-    { name: "ZoneSync", link: "https://ajoealex.github.io/ZoneSync/"}
+    { name: "ZoneSync", link: "https://ajoealex.github.io/ZoneSync/"},
+    { name: "API Test", link: "http://api-test.automationhub.in/"}
 ];
